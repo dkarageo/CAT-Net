33cdb4204b285b8f255fd3218e7e984adb0c4557
Dimitris Karageorgiou <dkarageo@iti.gr>
2023-05-16 23:14:33 +0300

    Refactoring of infer.py

--- tools/infer.py
+++ tools/infer.py
@@ -1,23 +1,24 @@
+"""Script for computing the outputs of CATNetv2 on a dataset.
+
+Version: 2.0.0
+
+Updated by Dimitris Karageorgiou
+email: dkarageo@iti.gr
+May 2023
+
+Created by Myung-Joon Kwon
+mjkwon2021@gmail.com
+June 7, 2021
 """
- Created by Myung-Joon Kwon
- mjkwon2021@gmail.com
- June 7, 2021
-"""
-import sys, os
+import os
+import sys
+
 path = os.path.join(os.path.dirname(os.path.realpath(__file__)), '..')
 if path not in sys.path:
     sys.path.insert(0, path)
 
 import argparse
-import pprint
-import shutil
 
-import logging
-import time
-import timeit
-from pathlib import Path
-
-import numpy as np
 from tqdm import tqdm
 
 import torch
@@ -25,19 +26,18 @@ import torch.nn as nn
 import torch.backends.cudnn as cudnn
 from torch.nn import functional as F
 
-from lib import models
 from lib.config import config
 from lib.config import update_config
 from lib.core.criterion import CrossEntropy, OhemCrossEntropy
-from lib.core.function import train, validate
-from lib.utils.modelsummary import get_model_summary
-from lib.utils.utils import create_logger, FullModel, get_rank
+from lib.utils.utils import FullModel
 
 from Splicing.data.data_core import SplicingDataset as splicing_dataset
-from pathlib import Path
 from project_config import dataset_paths
-import seaborn as sns; sns.set_theme()
+import seaborn as sns
+
+sns.set_theme()
 import matplotlib.pyplot as plt
+
 
 def parse_args():
     parser = argparse.ArgumentParser(description='Train segmentation network')
@@ -62,8 +62,18 @@ def main():
     # Instead of using argparse, force these args:
 
     ## CHOOSE ##
-    args = argparse.Namespace(cfg='experiments/CAT_full.yaml', opts=['TEST.MODEL_FILE', 'output/splicing_dataset/CAT_full/CAT_full_v2.pth.tar', 'TEST.FLIP_TEST', 'False', 'TEST.NUM_SAMPLES', '0'])
-    # args = argparse.Namespace(cfg='experiments/CAT_DCT_only.yaml', opts=['TEST.MODEL_FILE', 'output/splicing_dataset/CAT_DCT_only/DCT_only_v2.pth.tar', 'TEST.FLIP_TEST', 'False', 'TEST.NUM_SAMPLES', '0'])
+    args = argparse.Namespace(
+        cfg='experiments/CAT_full.yaml',
+        opts=['TEST.MODEL_FILE', 'output/splicing_dataset/CAT_full/CAT_full_v2.pth.tar',
+              'TEST.FLIP_TEST', 'False',
+              'TEST.NUM_SAMPLES', '0']
+    )
+    # args = argparse.Namespace(
+    #     cfg='experiments/CAT_DCT_only.yaml',
+    #     opts=['TEST.MODEL_FILE', 'output/splicing_dataset/CAT_DCT_only/DCT_only_v2.pth.tar',
+    #           'TEST.FLIP_TEST', 'False',
+    #           'TEST.NUM_SAMPLES', '0']
+    # )
     update_config(config, args)
 
     # cudnn related setting
@@ -72,8 +82,12 @@ def main():
     cudnn.enabled = config.CUDNN.ENABLED
 
     ## CHOOSE ##
-    test_dataset = splicing_dataset(crop_size=None, grid_crop=True, blocks=('RGB', 'DCTvol', 'qtable'), DCT_channels=1, mode='arbitrary', read_from_jpeg=True)  # full model
-    # test_dataset = splicing_dataset(crop_size=None, grid_crop=True, blocks=('DCTvol', 'qtable'), DCT_channels=1, mode='arbitrary', read_from_jpeg=True)  # DCT stream
+    test_dataset = splicing_dataset(crop_size=None, grid_crop=True,
+                                    blocks=('RGB', 'DCTvol', 'qtable'), DCT_channels=1,
+                                    mode='arbitrary', read_from_jpeg=True)  # full model
+    # test_dataset = splicing_dataset(crop_size=None, grid_crop=True,
+    #                                 blocks=('DCTvol', 'qtable'), DCT_channels=1,
+    #                                 mode='arbitrary', read_from_jpeg=True)  # DCT stream
 
     print(test_dataset.get_info())
 
@@ -82,7 +96,8 @@ def main():
         batch_size=1,  # must be 1 to handle arbitrary input sizes
         shuffle=False,  # must be False to get accurate filename
         num_workers=1,
-        pin_memory=False)
+        pin_memory=False
+    )
 
     # criterion
     if config.LOSS.USE_OHEM:
@@ -94,8 +109,7 @@ def main():
         criterion = CrossEntropy(ignore_label=config.TRAIN.IGNORE_LABEL,
                                  weight=test_dataset.class_weights).cuda()
 
-    model = eval('models.' + config.MODEL.NAME +
-                 '.get_seg_model')(config)
+    model = eval('models.' + config.MODEL.NAME + '.get_seg_model')(config)
     if config.TEST.MODEL_FILE:
         model_state_file = config.TEST.MODEL_FILE
     else:
@@ -109,7 +123,6 @@ def main():
     model = nn.DataParallel(model, device_ids=gpus).cuda()
 
     dataset_paths['SAVE_PRED'].mkdir(parents=True, exist_ok=True)
-
 
     def get_next_filename(i):
         dataset_list = test_dataset.dataset_list
@@ -143,7 +156,7 @@ def main():
                 width = pred.shape[1]  # in pixels
                 fig = plt.figure(frameon=False)
                 dpi = 40  # fig.dpi
-                fig.set_size_inches(width / dpi, ((width * pred.shape[0])/pred.shape[1]) / dpi)
+                fig.set_size_inches(width / dpi, ((width * pred.shape[0]) / pred.shape[1]) / dpi)
                 sns.heatmap(pred, vmin=0, vmax=1, cbar=False, cmap='gray', )
                 plt.axis('off')
                 plt.savefig(filepath, bbox_inches='tight', transparent=True, pad_inches=0)
